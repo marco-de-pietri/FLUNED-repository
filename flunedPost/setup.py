8019e16ae138302f5552a1f8d070b3b6d46b7513
marco-de-pietri <marco.deppo@hotmail.it>
2024-12-03 19:08:37 +0100

    new deps

--- flunedPost/setup.py
+++ flunedPost/setup.py
@@ -9,5 +9,13 @@ setuptools.setup(
             author_email="mdepietri@ind.uned.es",
             description="post processing of fluned simuations",
             packages = setuptools.find_packages(),
-            entry_points = {'console_scripts': ['flunedPost = flunedPost.flunedPost:main']})
+            entry_points = {'console_scripts': ['flunedPost = flunedPost.flunedPost:main']},
+            install_requires=[
+            "h5py",
+            "numpy>=1.21,<2", 
+            "vtk",
+            "pyvista",
+            ],
+            python_requires=">=3.11",  # Specify supported Python versions
+            )
          
