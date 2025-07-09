9e828c2264262cf37600b6f5fe8b7f50bb94aff2
marco-de-pietri <marco.deppo@hotmail.it>
2025-07-09 15:46:14 -0400

    initial documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,31 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'FLUNED'
-copyright = '2025, Marco De Pietri'
-author = 'Marco De Pietri'
-release = 'v1.0'
+
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+project = "FLUNED"
+copyright = "2025, Marco De Pietri"
+author = "Marco De Pietri"
+
+version = "1.0"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = []
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. FLUNED documentation master file, created by
-   sphinx-quickstart on Tue Jul  8 16:56:45 2025.
+   sphinx-quickstart on Wed Jul  9 15:05:01 2025.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
