5a9e9fa6a61b9b836f84135403410db0473d0aee
marco-de-pietri <marco.deppo@hotmail.it>
2025-07-09 18:40:05 -0400

    documentation skeleton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ import sys
 
 
 sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../..", "src"))
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
 )
 
 try:
