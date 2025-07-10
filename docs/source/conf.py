e72eb3a3d7f7b5637773afeb0df5d033e93f7b76
marco-de-pietri <marco.deppo@hotmail.it>
2025-07-10 09:54:02 -0400

    documentation skeleton fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,22 +7,31 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 
-import os
 import sys
+import importlib
+from pathlib import Path
 
 
-sys.path.insert(
-    0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
-)
+ROOT = Path(__file__).resolve().parents[2]   
+sys.path.insert(0, str(ROOT / "src"))
 
-try:
-    from importlib.metadata import version as _get_version
-except ImportError:
-    from pkg_resources import get_distribution as _get_version
+pkg_name = "fluned"                      # src/modulename/__init__.py
+pkg = importlib.import_module(pkg_name)
 
-release = _get_version("fluned")
-# short X.Y version
-version = ".".join(release.split(".")[:2])
+release = version = pkg.__version__          # e.g., "1.4.3"
+
+# sys.path.insert(
+#     0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
+# )
+#
+# try:
+#     from importlib.metadata import version as _get_version
+# except ImportError:
+#     from pkg_resources import get_distribution as _get_version
+#
+# release = _get_version("fluned")
+# # short X.Y version
+# version = ".".join(release.split(".")[:2])
 
 project = "FLUNED"
 copyright = "2025, Marco De Pietri"
